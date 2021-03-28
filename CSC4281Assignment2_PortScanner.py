a27f958d77919914f86333457effd87bf546178a
ecb7r <81536765+ecb7r@users.noreply.github.com>
2021-03-28 18:14:57 -0400

    Add files via upload

---
+++ CSC4281Assignment2_PortScanner.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 14 14:32:30 2021
+
+@author: Eric Burns
+
+This program is designed to scan an IP address for all open ports
+and list each of them.
+
+In my case, the target is on a pfsense internal network so the VM running
+this program must be ran on the same internal network or you won't be able to scan 
+it. The program uses the socket library to use sockets to scan ports
+and it also uses the system library to exit the program if Ctrl+C is pressed or
+it closes if it has issues connecting to the target.
+
+The program must be ran from the terminal in the same directory using the command
+sudo python3 [this program's name].py -i
+The above command will prompt you for the IP address, enter it and press enter
+to start the scan. If anything else is entered, it will give an error message for invalid input.
+
+"""
+
+import socket #import socket library
+import sys #import system library
+
+target = '' #create a target variable to store IP later.
+
+if(len(sys.argv) == 2 and sys.argv[1] == '-i'): #if the program is ran through terminal with a -i
+    print("Please enter the IP address of the target to scan: ") #ask for the IP address
+    target = input() #put the IP address input into the target variable
+
+elif(len(sys.argv) == 1): #if the program isn't ran with a -i
+    print("Please enter '-i' to enter an IP address to scan.") #print an error
+    sys.exit() #and exit the program
+    
+else: #otherwise if anything else is ran
+    print("Invalid input, please try again!") #print an error
+    sys.exit() #and exit the program
+
+try: #let's try to find some open ports
+    
+    for port in range(1,65535): #for each port number in the whole range
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #let's create a socket
+        socket.setdefaulttimeout(1) #create a timeout 
+        
+        result = s.connect_ex((target, port)) #result variable to store the socket value returned
+        if result == 0: #if the result returned is 0
+            print('Port ', port, ' is open.') #we print that that port is open 
+        s.close() #then close the socket
+        
+except KeyboardInterrupt: #when the user presses Ctrl + C
+    print("\n Closing...") #print a closing message
+    sys.exit() #and exit
+    
+except socket.error: #if there's a socket error
+    print("\n Issues connecting to target.") #print an error message
+    sys.exit() #and exit
---
+++ Shadow_Pass_Cracker.py
@@ -0,0 +1,275 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  4 22:18:23 2021
+
+@author: Eric Burns
+
+This tool is used to crack passwords from a shadow file by using any
+wordlist you put a path for. I removed the Blowfish and Crypt algorithms methods
+from the functions.
+
+Python 3.7 language
+
+Instructions: to run the program, go to the directory where this file is saved,
+then type sudo python3 [this program's name] [directory of shadow file] [directory of wordlist]
+you can also add a '-m' to the end to get the mangle rules which check to see if the password
+is capitalized or if it's capitalized and has a special character at the end.
+"""
+
+import sys #import sys library
+import crypt #import crypt library
+ 
+def wordlistChecker(shadowHash, wordlist): #function to compare wordlist hash with shadow file hashes
+    
+    try: #lets try to open the wordlist given
+        
+        wlist = open(wordlist, 'r') #open the wordlist designated earlier
+    
+    except: #if it won't open or wrong path lets print an error and close the program
+        
+        print("Wordlist wasn't found or path was incorrect!") #error message
+        sys.exit() #close the program if the try fails
+        
+    cryptType = shadowHash.split("$")[1] #get the encryption ID
+    
+    if cryptType == '6': #if the encryption ID is 6 then it runs this loop
+        
+        print("Algorithm: SHA-512") #prints that the algorithm is SHA-512
+        salt = shadowHash.split("$")[2] #finds the salt after the encryption ID
+        insalt = "$" + cryptType + "$" + salt + "$" #creates the whole salt to crypt.crypt later
+        
+        for password in wlist.readlines(): #for loop to read through the wordlist
+            
+            password = password.strip('\n') #strip the new lines
+            sha512_passwordHash = crypt.crypt(password, insalt) #get the SHA-512 hash to compare
+                                                                #with the shadow's hash
+            
+            if(sha512_passwordHash == shadowHash): #if the hashes are equal
+                
+                print("Password: " + password) #print the password
+                print("____________________\n") #just to separate the user, algorithm, and password
+        
+    if cryptType == '5': #if the encryption ID is 5 then it runs this loop
+        
+        print("Algorithm: SHA-256") #prints that the algorithm is SHA-256
+        salt = shadowHash.split("$")[2] #finds the salt after the encryption ID
+        insalt = "$" + cryptType + "$" + salt + "$" #creates the whole salt to crypt.crypt later
+        
+        for password in wlist.readlines(): #for loop to read through the wordlist
+            
+            password = password.strip('\n')  #strip the new lines
+            sha256_passwordHash = crypt.crypt(password, insalt) #get the SHA-256 hash to compare
+                                                                #with the shadow's hash
+            
+            if(sha256_passwordHash == shadowHash): #if the hashes are equal
+                
+                print("Password: " + password) #print the password
+                print("____________________\n") #just to separate the user, algorithm, and password
+                
+    if cryptType == '1': #if the encryption ID is 1 then it runs this loop
+        
+        print("Algorithm: MD5") #prints that the algorithm is MD5
+        salt = shadowHash.split("$")[2] #finds the salt after the encryption ID
+        insalt = "$" + cryptType + "$" + salt + "$" #creates the whole salt to crypt.crypt later
+        
+        for password in wlist.readlines(): #for loop to read through the wordlist
+            
+            password = password.strip('\n') #strip the new lines
+            MD5_passwordHash = crypt.crypt(password, insalt) #get the MD5 hash to compare
+                                                                #with the shadow's hash
+            
+            if(MD5_passwordHash == shadowHash): #if the hashes are equal
+                
+                print("Password: " + password) #print the password
+                print("____________________\n") #just to separate the user, algorithm, and password
+
+def manglerListChecker(shadowHash, wordlist): #function called for mangling rules
+    
+    try: #lets try to open the wordlist given
+        
+        wlist = open(wordlist, 'r') #open the wordlist designated earlier
+    
+    except: #if it won't open or wrong path lets print an error and close the program
+        
+        print("Wordlist wasn't found or path was incorrect!") #error message
+        sys.exit() #close the program if the try fails
+        
+    cryptType = shadowHash.split("$")[1] #get the encryption ID
+    
+    if cryptType == '6': #if the encryption ID is 6 then it runs this loop
+        
+        print("Algorithm: SHA-512") #prints that the algorithm is SHA-512
+        salt = shadowHash.split("$")[2] #finds the salt after the encryption ID
+        insalt = "$" + cryptType + "$" + salt + "$" #creates the whole salt to crypt.crypt later
+        
+        for password in wlist.readlines(): #for loop to read through the wordlist
+            
+            password = password.strip('\n') #strip the new lines
+            sha512_passwordHash = crypt.crypt(password, insalt) #get the SHA-512 hash to compare
+                                                                #with the shadow's hash
+            
+            if(sha512_passwordHash == shadowHash): #if the hashes are equal
+                
+                print("Password: " + password) #print the password
+                print("____________________\n") #just to separate the user, algorithm, and password
+                break
+            
+            else: #if the hashes aren't equal lets check other options
+                
+                i = int(1) #here to help increment the ascii values
+                password = password.capitalize() #capitalize to check
+                sha512_passwordHash = crypt.crypt(password, insalt) #get the SHA-512 hash to compare
+                                                                #with the shadow's hash
+                
+                if(sha512_passwordHash == shadowHash): #if the hashes are equal
+                   
+                    print("Password: " + password) #print the password
+                    print("____________________\n") #just to separate the user, algorithm, and password
+                    break
+                
+                for i in range(15): #for loop to check each special ascii character with each word
+                    
+                    password = password + chr(33 + i) #increment the ascii number to see all specials
+                    sha512_passwordHash = crypt.crypt(password, insalt) #get the SHA-512 hash to compare
+                                                                #with the shadow's hash
+                    
+                    if(sha512_passwordHash == shadowHash): #if the hashes are equal
+                        
+                        print("Password: " + password) #print the password
+                        print("____________________\n") #just to separate the user, algorithm, and password
+                        break
+                    
+                    password = password[:-1] #this is here to remove special characters every time the loop runs
+        
+    if cryptType == '5': #if the encryption ID is 5 then it runs this loop
+        
+        print("Algorithm: SHA-256") #prints that the algorithm is SHA-256
+        salt = shadowHash.split("$")[2] #finds the salt after the encryption ID
+        insalt = "$" + cryptType + "$" + salt + "$" #creates the whole salt to crypt.crypt later
+        
+        for password in wlist.readlines(): #for loop to read through the wordlist
+            
+            password = password.strip('\n') #strip the new lines
+            sha256_passwordHash = crypt.crypt(password, insalt) #get the SHA-256 hash to compare
+                                                                #with the shadow's hash
+            
+            if(sha256_passwordHash == shadowHash): #if the hashes are equal
+                
+                print("Password: " + password) #print the password
+                print("____________________\n") #just to separate the user, algorithm, and password
+                break
+
+            else: #if the hashes aren't equal lets check other options
+                
+                i = int(1)
+                password = password.capitalize() #capitalize to check
+                sha256_passwordHash = crypt.crypt(password, insalt) #get the SHA-256 hash to compare
+                                                                #with the shadow's hash
+                
+                if(sha256_passwordHash == shadowHash): #if the hashes are equal
+                    
+                    print("Password: " + password) #print the password
+                    print("____________________\n") #just to separate the user, algorithm, and password
+                    break
+                
+                for i in range(15): #for loop to check each special ascii character with each word
+                    
+                    password = password + chr(33 + i) #increment the ascii number to see all specials
+                    sha256_passwordHash = crypt.crypt(password, insalt) #get the SHA-256 hash to compare
+                                                                #with the shadow's hash
+                    
+                    if(sha256_passwordHash == shadowHash): #if the hashes are equal
+                        
+                        print("Password: " + password) #print the password
+                        print("____________________\n") #just to separate the user, algorithm, and password
+                        break
+                    
+                    password = password[:-1] #this is here to remove special characters every time the loop runs
+
+    if cryptType == '1': #if the encryption ID is 1 then it runs this loop
+        
+        print("Algorithm: MD5") #prints that the algorithm is MD5
+        salt = shadowHash.split("$")[2] #finds the salt after the encryption ID
+        insalt = "$" + cryptType + "$" + salt + "$" #creates the whole salt to crypt.crypt later
+        
+        for password in wlist.readlines(): #for loop to read through the wordlist
+           
+            password = password.strip('\n') #strip the new lines
+            MD5_passwordHash = crypt.crypt(password, insalt) #get the MD5 hash to compare
+                                                                #with the shadow's hash
+            
+            if(MD5_passwordHash == shadowHash): #if the hashes are equal
+                
+                print("Password: " + password) #print the password
+                print("____________________\n") #just to separate the user, algorithm, and password
+                break
+            
+            else: #if the hashes aren't equal lets check other options
+                
+                i = int(1)
+                password = password.capitalize() #capitalize to check
+                MD5_passwordHash = crypt.crypt(password, insalt) #get the MD5 hash to compare
+                                                                #with the shadow's hash
+                
+                if(MD5_passwordHash == shadowHash): #if the hashes are equal
+                    
+                    print("Password: " + password) #print the password
+                    print("____________________\n") #just to separate the user, algorithm, and password
+                    break
+                
+                for i in range(15): #for loop to check each special ascii character with each word
+                    
+                    password = password + chr(33 + i) #increment the ascii number to see all specials
+                    MD5_passwordHash = crypt.crypt(password, insalt) #get the MD5 hash to compare
+                                                                #with the shadow's hash
+                    
+                    if(MD5_passwordHash == shadowHash): #if the hashes are equal
+                        
+                        print("Password: " + password) #print the password
+                        print("____________________\n") #just to separate the user, algorithm, and password
+                        break
+                    
+                    password = password[:-1] #this is here to remove special characters every time the loop runs
+
+def main(): #this is our main function to start the program
+    
+    shadowPath = sys.argv[1] #stores the path to the shadow file
+    wordlist = sys.argv[2] #stores the path to the wordlist
+    
+    try: #lets try to open the shadow file given
+        
+        shadowFile = open(shadowPath, 'r') #open shadow file and read it
+    
+    except: #if it won't open or wrong path lets print an error and close the program
+        
+        print("File wasn't found or path was incorrect!") #error message
+        sys.exit() #close the program if the try fails
+    
+    print("____________________\n") #adds some space between the terminal command and the first user
+    if(len(sys.argv) == 3): #if the mangle rules aren't used    
+        
+        for shadowLines in shadowFile.readlines(): #for loop to read each line in the shadow file
+            shadowLines = shadowLines.replace("\n","").split(":") #replace the new line in shadow and split
+            
+            if not shadowLines[1] in ['x', '*', '!']: #if loop excluding these characters
+                userStore = shadowLines[0] #store the username after splitting at :
+                shadowHash = shadowLines[1] #store the hash after splitting
+                print("Username: " + userStore) #print the username
+                wordlistChecker(shadowHash, wordlist) #call wordlistChecker function
+
+    elif(len(sys.argv) == 4 and sys.argv[3] == '-m'): #otherwise if we use the mangle rules
+        
+         for shadowLines in shadowFile.readlines(): #for loop to read each line in the shadow file   
+             shadowLines = shadowLines.replace("\n","").split(":") #replace the new line in shadow and split
+             
+             if not shadowLines[1] in ['x', '*', '!']: #if loop excluding these characters
+                userStore = shadowLines[0] #store the username after splitting at :
+                shadowHash = shadowLines[1] #store the hash after splitting
+                print("Username: " + userStore) #print the username
+                manglerListChecker(shadowHash, wordlist) #call manglerListChecker function
+
+ 
+ 
+
+main() #call the main function
+
